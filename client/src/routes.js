13c130ede7ff7bf7a1dfcde6b0aa33809fc0ff99
Daniel Rabinovich <drabin89@gmail.com>
2016-09-24 19:37:52 -0700

    added splashpage component

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from './components/App.jsx';
+import SplashPage from './components/splash/splashPage.jsx';
 import LoginPage from './components/login-signup/LoginPage.jsx';
 import SignupPage from './components/login-signup/SignupPage.jsx';
 import LogoutPage from './components/login-signup/LogoutPage.jsx';
@@ -10,6 +11,7 @@ import DashboardPage from './components/dashboard/DashboardPage.jsx';
 export default (
   <Route path="/" component={App}>
     <Route path="/login" component={LoginPage} />
+    <IndexRoute component={SplashPage} />
     <Route path="/signup" component={SignupPage} />
     <Route path="/logout" component={LogoutPage} />
     <Route path="/dashboard" component={requireAuth(DashboardPage)} />
